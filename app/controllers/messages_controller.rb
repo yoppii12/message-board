b4d1a602b506d33b6b4c2be91f5c5619a61e1ee5
yoppii12 <nya.nya.yoska@gmail.com>
2015-09-07 07:27:43 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    #Messageをすべて取得する。
+    @messages=Message.all
   end
+  
+  ##ここから表記
+  def create
+    @message=Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました。'
+  end
+  
+  ##ココから下はprivateメソッドとなる
+  private
+  def message_params
+    #params[:message]のパラメータでname, bodyのみ許可する
+    #返り値はex:){name:"入力されたname",body:"入力されたbody"}
+    params.require(:message).permit(:name, :body)
+  end
+  ##ここまで
 end
