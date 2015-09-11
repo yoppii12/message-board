ee9bdf2dacfa0c98543f9b4be7d6e3bb7d38036e
yoppii12 <nya.nya.yoska@gmail.com>
2015-09-11 06:06:38 +0000

    delete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     #Messageをすべて取得する。
@@ -8,6 +8,11 @@ class MessagesController < ApplicationController
   end
   
   def edit
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   def update
